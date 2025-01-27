3e82c114dcd79eea31b356687f3b984a6cb2338a
Hifza Mehboob <hifzamehboob771@gmail.com>
2025-01-27 17:41:10 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+num1:int = 50
+num2:int = 40
+def addition ():
+  print (num1+num2)
+addition()
+
+def subtraction ():
+  print (num1-num2)
+subtraction()
+
+def multiplication():
+  print (num1*num2)
+multiplication()
+
+def division():
+  print (num1/num2)
+division()
